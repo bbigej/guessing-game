8559248cd20d376bc09e9ab9e23d7f591409f7a9
Bryan James Bigej <bbigej@gmail.com>
2017-08-06 15:13:09 -0700

    implemented 4th question, added comments.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var userName = prompt("Hi, what's your name?");
 alert("Hi " + userName + ", I hope you're ready to party.");
+
+//first question
 var colorQuestion = prompt("Is my favorite color blue?");
 console.log("The answer is blue.");
 if(colorQuestion == "Yes" || colorQuestion == "yes") {
@@ -7,6 +9,7 @@ if(colorQuestion == "Yes" || colorQuestion == "yes") {
 } else {
     alert("Sorry. Blue is indeed my favorite color, " + userName + ".");
 }
+//second question
 var petQuestion = prompt("Okay, next question. Do I own a cat?"); 
 console.log("The answer is yes, I do have a cat.");
 if (petQuestion == "Yes" || petQuestion == "yes") {
@@ -14,10 +17,23 @@ alert("Good job " + userName + ", I do have a cat named Chloe.");
 } else {
     alert(userName + ", that isn't correct. I actually do have a cat.");
 }
-var homeQuestion = prompt("Last question: do I want to live in Hawaii?");
+//third question
+var homeQuestion = prompt("Do I want to live in Hawaii?");
 console.log("Yes, I would love to live in Hawaii.");
 if (homeQuestion == "Yes" || homeQuestion == "yes") {
     alert("You're right, that would be awesome " + userName + ".");
 } else {
     alert("False! I would love to live in Hawaii, " + userName + "!");
+}
+//fourth question
+var travelQuestion = prompt("How many countries have I traveled to?");
+console.log("The answer is 7.");
+if (travelQuestion <= 6) {
+    alert("Your answer is too low, " + userName + ".");
+} 
+else if (travelQuestion >= 8) {
+    alert("Your answer is too high, " + userName + ".");
+}
+else {
+alert("Good guess " + userName +", that's right.");
 }
