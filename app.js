370a4efc0783b4f7fa95ea72e4716cbb3578a446
Bryan James Bigej <bbigej@gmail.com>
2017-08-08 20:15:41 -0700

    implemented the rest of the questions as called functions

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-var userName = prompt("Hi, what's your name?");
+// ***** get the user's name  prompt *****
+//var userName = prompt("Hi, what's your name?");
+//alert("Hi " + userName + ", welcome to my page.");
+
+
+//get the user's name function
+
+
+
+function getName() {
+    var userName = prompt("Hi, what's your name?");
 alert("Hi " + userName + ", welcome to my page.");
+return userName;
+}
+
+var userName = getName();
 
 var questionCounter = 0;   /*this will keep track of how many answers are 
 correct by increasing +1 for each correct answer*/
@@ -23,11 +37,22 @@ if(colorQuestion.toUpperCase() == "YES" || colorQuestion.toUpperCase() =="Y") {
     alert("Good job " + userName + ", that is my favorite color.");
 } else {
     alert("Sorry. Blue is indeed my favorite color, " + userName + ".");
-}
+    }
 }
 firstQuestion();
 
-//second question
+//  ***** second question prompts *****
+/*var petQuestion = prompt("Okay, next question. Do I own a cat?"); 
+console.log("The answer is yes, I do have a cat.");
+if (petQuestion.toUpperCase() == "YES" || petQuestion.toUpperCase() == "Y") {
+    questionCounter = questionCounter + 1;
+alert("Good job " + userName + ", I do have a cat named Chloe.");
+} else {
+    alert(userName + ", that isn't correct. I actually do have a cat.");
+} */
+
+// second question function
+function secondQuestion() {
 var petQuestion = prompt("Okay, next question. Do I own a cat?"); 
 console.log("The answer is yes, I do have a cat.");
 if (petQuestion.toUpperCase() == "YES" || petQuestion.toUpperCase() == "Y") {
@@ -35,8 +60,22 @@ if (petQuestion.toUpperCase() == "YES" || petQuestion.toUpperCase() == "Y") {
 alert("Good job " + userName + ", I do have a cat named Chloe.");
 } else {
     alert(userName + ", that isn't correct. I actually do have a cat.");
+    }
 }
-//third question
+secondQuestion();
+
+// ***** third question prompts *****
+/*var homeQuestion = prompt("Do I want to live in Hawaii?");
+console.log("Yes, I would love to live in Hawaii.");
+if (homeQuestion.toUpperCase() == "YES" || homeQuestion.toUpperCase() == "Y") {
+    questionCounter = questionCounter + 1;
+    alert("You're right, that would be awesome " + userName + ".");
+} else {
+    alert("False! I would love to live in Hawaii, " + userName + "!");
+}*/
+
+//third question function
+function thirdQuestion() {
 var homeQuestion = prompt("Do I want to live in Hawaii?");
 console.log("Yes, I would love to live in Hawaii.");
 if (homeQuestion.toUpperCase() == "YES" || homeQuestion.toUpperCase() == "Y") {
@@ -44,9 +83,12 @@ if (homeQuestion.toUpperCase() == "YES" || homeQuestion.toUpperCase() == "Y") {
     alert("You're right, that would be awesome " + userName + ".");
 } else {
     alert("False! I would love to live in Hawaii, " + userName + "!");
+    }
 }
-//fourth question
-var travelQuestion = prompt("How many countries have I traveled to?");
+thirdQuestion();
+
+// ***** fourth question prompts *****
+/*var travelQuestion = prompt("How many countries have I traveled to?");
 console.log("The answer is 7.");
 while (travelQuestion !== 7) {
 if (travelQuestion <= 6) {
@@ -62,5 +104,28 @@ else {
 alert("Good guess " + userName +", that's right.");
 travelQuestion = 7
 }
+} */
+
+function fourthQuestion() {
+var travelQuestion = prompt("How many countries have I traveled to?");
+console.log("The answer is 7.");
+while (travelQuestion !== 7) {
+if (travelQuestion <= 6) {
+    alert("Your answer is too low, " + userName + ".");
+    travelQuestion = prompt("Give it another shot.");
+} 
+else if (travelQuestion >= 8) {
+    alert("Your answer is too high, " + userName + ".");
+     travelQuestion = prompt("Give it another shot.");
 }
+else {
+    questionCounter = questionCounter + 1;
+alert("Good guess " + userName +", that's right.");
+travelQuestion = 7
+        }
+    }
+}
+
+fourthQuestion();
+
 alert("You got " + questionCounter + " questions correct.");
