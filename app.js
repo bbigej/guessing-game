1650247e918c6e7d4779bcb5421622dbc5973949
Bryan James Bigej <bbigej@gmail.com>
2017-08-13 17:48:26 -0700

    implemented array question

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 
-
 //Code to load messages into the index.html document
 var message = "Special Instructions for User";
 
 function updatePage() {
     document.getElementById('game').innerHTML = message;
 }
-
 
 // getting the user's name function
 function getName() {
@@ -17,61 +15,37 @@ function getName() {
 
 var userName = getName();
 
-updatePage();
-
-var questionCounter = 0;   /*this will keep track of how many answers are 
+/*this will keep track of how many answers are 
 correct by increasing +1 for each correct answer*/
+var questionCounter = 0;  
 
-//first question function
-function firstQuestion() {
-    var colorQuestion = prompt("Is my favorite color blue?");
-    console.log("The answer is blue.");
-    if(colorQuestion.toUpperCase() == "YES" || colorQuestion.toUpperCase() =="Y") {
+//function for yes/no questions
+function yesNoQuestion(question, rightAnswer, wrongAnswer) {
+    var response = prompt(question);
+    if (response.toUpperCase() == "YES" || response.toUpperCase() == "Y") {
     questionCounter = questionCounter + 1;
-    message += "Good job " + userName + ", that is my favorite color." + "<br>";
+    message += rightAnswer;
     } else {
-    message += "Sorry. Blue is indeed my favorite color, " + userName + "." + "<br>";
+    message += wrongAnswer;
     }
 }
 
-firstQuestion();
+//question one
+yesNoQuestion("Is my favorite color blue?", 
+"Good job " + userName + ", that is my favorite color." + "<br>",
+"Sorry. Blue is indeed my favorite color, " + userName + "." + "<br>",);
 
-updatePage();
+//question two
+yesNoQuestion("Okay, next question. Do I own a cat?", 
+"Good job " + userName + ", I do have a cat named Chloe." + "<br>",
+userName + ", that isn't correct. I actually do have a cat.");
 
-// second question function
-function secondQuestion() {
-    var petQuestion = prompt("Okay, next question. Do I own a cat?"); 
-    console.log("The answer is yes, I do have a cat.");
-    if (petQuestion.toUpperCase() == "YES" || petQuestion.toUpperCase() == "Y") {
-    questionCounter = questionCounter + 1;
-    message += "Good job " + userName + ", I do have a cat named Chloe." + "<br>";
-    } else {
-    message += userName + ", that isn't correct. I actually do have a cat." + "<br>";
-    }
-}
+//question three
+yesNoQuestion("Do I want to live in Hawaii?", 
+"You're right, that would be awesome " + userName + "." + "<br>",
+"False! I would love to live in Hawaii, " + userName + "!" + "<br>",);
 
-secondQuestion();
-
-updatePage();
-
-
-//third question function
-function thirdQuestion() {
-    var homeQuestion = prompt("Do I want to live in Hawaii?");
-    console.log("Yes, I would love to live in Hawaii.");
-    if (homeQuestion.toUpperCase() == "YES" || homeQuestion.toUpperCase() == "Y") {
-    questionCounter = questionCounter + 1;
-    message += "You're right, that would be awesome " + userName + "." + "<br>";
-    } else {
-    message += "False! I would love to live in Hawaii, " + userName + "!" + "<br>";
-    }
-}
-
-thirdQuestion();
-
-updatePage();
-
-//fourth question function
+//question four
 function fourthQuestion() {
     var travelQuestion = prompt("How many countries have I traveled to?");
     console.log("The answer is 7.");
@@ -94,8 +68,27 @@ function fourthQuestion() {
 
 fourthQuestion();
 
-updatePage();
+var fifthQuestionAnswers = ["France", "England", "Thailand", "Taiwan",
+"Canada", "Netherlands"]
 
-message += "You got " + questionCounter + " questions correct.";
+//question 5
+function fifthQuestion() {
+    var response = prompt("Where else in the world have I been?");
+    if (response == fifthQuestionAnswers[0] ||
+        response == fifthQuestionAnswers[1] ||
+        response == fifthQuestionAnswers[2] ||
+        response == fifthQuestionAnswers[3] ||
+        response == fifthQuestionAnswers[4] ||
+        response == fifthQuestionAnswers[5])    {
+    questionCounter = questionCounter + 1;
+    message += "Good guess, " + userName + ", it was lovely."+ "<br>";
+    } else {
+    message += "I haven't been there yet, " + userName + "."+ "<br>";
+    }
+}
+
+fifthQuestion();
+
+message += "You answered " + questionCounter + " questions correctly.";
 
 updatePage();
